2eb6039c6953bdc7c6c83b45b1b76f9d728e3721
KatakamYoshitha <yoshithaka123@gmail.com>
2026-03-26 14:32:04 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "ecommerce-backend-production-12cf.up.railway.app"
+  baseURL: "https://ecommerce-backend-production-bcab.up.railway.app/"
 });
 
 export default API;
